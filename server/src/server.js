0bfbad042c0b2f1d15a6c80825fafe42aef54c39
MoralessJ <3647693780101@ingenieria.usac.edu.gt>
2024-12-22 22:29:42 -0600

    POSTMAN Export

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
+import { corsConfig } from "./config/cors.js";
 
 import orderRoutes from "./routes/orderRoutes.js";
 import detailRoutes from "./routes/detailRoutes.js";
@@ -11,6 +14,7 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const server = express();
+
 server.use(express.json());
 
 // ————— Rutas
